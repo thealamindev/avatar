0169a0f644e5fb7daa35057817f7bf24f8ab4c78
Al-Amin <ll.alamin.ll@gmail.com>
2023-01-06 18:49:40 +0600

    gft

---
+++ js/tailwind.config.js
@@ -0,0 +1,51 @@
+tailwind.config = {
+    theme: {
+        extend: {
+            colors: {
+                mainBtnColor: '#1BBF00',
+                headColor: '#141135',
+                overly: '#6B62C5'
+
+            },
+            maxWidth: {
+                navContainer: '1170px',
+                bannerContainer: '962px',
+            },
+
+            lineHeight: {
+                menuLineH: '70px',
+            },
+
+            borderRadius: {
+                btnradius: '20px',
+            },
+
+            margin: {
+                menuItem: '40px',
+            },
+            padding: {
+                bannerpt: '265px',
+                bannerpb: '250px',
+            },
+            fontFamily: {
+                open: ['Open Sans', 'sans-serif'],
+                nunito: ['Nunito', 'sans-serif'],
+                paprika: ['Paprika', 'cursive'],
+            },
+
+            backgroundImage: {
+                banner: "url('images/banner.jpg')",
+
+            },
+
+            fontSize: {
+                headFont: '84px',
+
+
+
+            }
+
+
+        }
+    }
+}
